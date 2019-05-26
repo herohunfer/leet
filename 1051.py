74a88d846718ced23a417a2ff7d487610363ab2c
herohunfer <herohunfer@gmail.com>
2019-05-26 00:55:04 -0400

    Update 1051.py

--- 1051.py
+++ 1051.py
@@ -1,4 +1,4 @@
 class Solution:
     def heightChecker(self, heights: List[int]) -> int:
         hsort = sorted(heights)
-        return sum([1 if heights[i] == hsort[i] else 0 for i in range(len(heights))])
+        return sum([1 if heights[i] != hsort[i] else 0 for i in range(len(heights))])
