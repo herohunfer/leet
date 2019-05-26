1a0452366f388c0007f7bef68cda85eaaf30a55a
herohunfer <herohunfer@gmail.com>
2019-05-26 00:45:49 -0400

    Update 1053.py

--- 1053.py
+++ 1053.py
@@ -1,11 +1,13 @@
 class Solution:
     def prevPermOpt1(self, A: List[int]) -> List[int]:
-        for i in range(0, len(A)-1, -1):
+        for i in range(len(A)-2, -1, -1):
             if A[i] > A[i+1]:
-                for j in range(0, len(A), -1):
+                for j in range(len(A)-1, i, -1):
+                    while j  > i and A[j] == A[j-1]:
+                        j -= 1
                     if A[i]> A[j]:
-                        temp = A[pos]
-                        A[pos] = A[i]
+                        temp = A[j]
+                        A[j] = A[i]
                         A[i] = temp
                         return A
         return A
