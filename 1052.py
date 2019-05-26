5cc84a75b69e4b67a0586f5b7e64b86106d5934b
herohunfer <herohunfer@gmail.com>
2019-05-26 00:52:32 -0400

    Update 1052.py

--- 1052.py
+++ 1052.py
@@ -1,11 +1,14 @@
 class Solution:
     def maxSatisfied(self, customers: List[int], grumpy: List[int], X: int) -> int:
-        
-        total = sum([customers[i] if not grumpy[i] for i in range(len(customers))])
-        current = sum([customers[i] if grumpy[i] for i in range(X)])
+        total = sum([customers[i] if not grumpy[i] else 0 for i in range(len(customers))])
+        # print(total)
+        current = sum([customers[i] if grumpy[i] else 0 for i in range(X)])
+        # print(current)
         maximum = current
-        for i in range(X, len(customers)-X+1):
-            current += (customers[i] * grumpy[i]) - (customers[i-X] * grumpy[i])
+        # print("current=",current)
+        for i in range(X, len(customers)):
+            current += (customers[i] * grumpy[i]) - (customers[i-X] * grumpy[i-X])
+            # print("current=",current)
             if current > maximum:
                 maximum = current
         return total + maximum
